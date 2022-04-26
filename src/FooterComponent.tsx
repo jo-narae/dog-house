2faeb6fabe1bed63b8984f822ec2fd4a78077ee9
jo-narae <nare6004@gmail.com>
2022-04-26 14:16:26 +0900

    first commit

---
+++ src/FooterComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './FooterComponent.scss';
+
+export default function FooterComponent() {
+    return (
+        <footer className="foot">
+            <p>Copyright 2012 funnycom reserved.</p>
+        </footer>
+    )
+}
